04a1e77a10743e0ed9b9966d54059dbce7044100
sjdhome <sjdhoome@gmail.com>
2026-08-20 10:30:15 +0800

    Initial Commit

---
+++ TmuxAgentWatchTests/TmuxAgentWatchTests.swift
@@ -0,0 +1,19 @@
+//
+//  TmuxAgentWatchTests.swift
+//  TmuxAgentWatchTests
+//
+//  Created by sjdhome on 2026/8/20.
+//
+
+import Testing
+@testable import TmuxAgentWatch
+
+struct TmuxAgentWatchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
